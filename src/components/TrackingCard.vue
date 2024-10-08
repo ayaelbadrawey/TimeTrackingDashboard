8734ee62b0958dd783ec380c33c2865d0ca94f21
ayaelbadrawey <ayaelbadrawey@gmail.com>
2024-10-08 16:24:29 +0300

    setup main parts

---
+++ src/components/TrackingCard.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+<template>
+    <div class="tracking-card">
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+.tracking-card {
+    background-color: #1C204B;
+    width: 255px;
+    height: 199px;
+    border-radius: 15px;
+    box-shadow: 0px -35px #FF5E7D;
+    margin: 25px 0;
+}
+
+@media (max-width: 1400px) and (min-width: 769px) {
+    .tracking-card {
+        width: auto;
+    }
+}
+
+@media (max-width: 768px) {
+    .tracking-card {
+        width: 100%;
+        margin-bottom: 50px;
+    }
+}
+</style>
---
+++ src/views/TimeTrackingDashboardView.vue
@@ -0,0 +1,95 @@
+<script setup>
+import TrackingCard from '../components/TrackingCard.vue'
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            <div class="best-sales">
+                <TrackingCard />>
+            </div>
+            <div class="tracking">
+                <TrackingCard v-for="i in 10"></TrackingCard>
+            </div>
+        </div>
+    </main>
+</template>
+
+
+
+<style lang="scss" scoped>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+main {
+    display: grid;
+    place-items: center;
+    height: 100vh;
+    width: 100vw;
+
+    .container {
+        display: grid;
+        grid-template-columns: repeat(12, 1fr);
+        gap: 20px;
+        width: 90%;
+    }
+
+    .best-sales {
+        grid-column: span 2;
+    }
+
+    .tracking {
+        grid-column: span 10;
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        gap: 10px;
+    }
+}
+
+@media (max-width: 1400px) and (min-width: 769px) {
+    main {
+        padding-top: 70px;
+        grid-template-columns: 1fr;
+
+        .container {
+            grid-template-columns: repeat(12, 1fr);
+        }
+
+        .best-sales {
+            grid-column: span 4;
+        }
+
+        .tracking {
+            grid-column: span 8;
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+        }
+    }
+}
+
+@media (max-width: 768px) {
+    main {
+        padding-top: 70px;
+        grid-template-columns: 1fr;
+        height: 100vh;
+
+        .container {
+            grid-template-columns: 1fr;
+            gap: 0;
+        }
+
+        .best-sales {
+            grid-column: span 1;
+            width: 100%;
+        }
+
+        .tracking {
+            grid-column: span 1;
+            display: block;
+        }
+    }
+}
+</style>
